73ccd14f74ebc3c7c524035509fed2021097f5b3
Nicolás Zarantonello <124157205+NicoZdev@users.noreply.github.com>
2023-02-10 01:36:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.write("");
+
+window.onload = function(){
+    var contenedor = document.getElementById('contcarga');
+
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
